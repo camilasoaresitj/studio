8513a0591e9b6c0333a5f15657e7790454e5c3d2
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-07-19 20:55:21 +0000

    NOT_FOUND: Model googleai/gemini-1.5-pro not found

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,11 @@
 
 import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
 // This file is imported by all flows.
 // It is safe to import in 'use server' files because it does not initialize any plugins.
 // The actual plugin initialization is done in `src/ai/dev.ts`, which is only used by the genkit CLI.
 export const ai = genkit({
-    model: 'gemini-1.5-pro',
+    plugins: [googleAI()],
+    model: 'gemini-1.5-flash',
 });

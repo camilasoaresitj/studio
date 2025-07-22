177d8a529014d7cce15d593255e1e26f5bc775ac
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-07-22 01:10:17 +0000

    Verifique a URL: Certifique-se de que a URL que você está tentando acess

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    optimizePackageImports: ['@googlemaps/js-api-loader']
-  },
   trailingSlash: true,
   skipTrailingSlashRedirect: true
 }

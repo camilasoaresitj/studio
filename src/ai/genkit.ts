df214f27b67cbfae912fdde169fee0e1d3b6fde6
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-07-22 22:10:59 +0000

    Unhandled Runtime Error Error: (0 , _genkit_ai_core__WEBPACK_IMPORTED_MO

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,12 @@
 
 import { genkit } from '@genkit-ai/core';
 import { googleAI } from '@genkit-ai/googleai';
-import { dotprompt } from '@genkit-ai/dotprompt';
 
 // This file is imported by all flows.
 // It is safe to import in 'use server' files because it does not initialize any plugins.
 // The actual plugin initialization is done in `src/ai/dev.ts`, which is only used by the genkit CLI.
 export const ai = genkit({
-    plugins: [googleAI(), dotprompt()],
+    plugins: [googleAI()],
     models: {
         'gemini-pro': {
             path: 'gemini-1.5-flash-latest',

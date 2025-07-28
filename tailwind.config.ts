4445b609235d15b67970cfdd154a0c0f98b737d5
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-07-28 23:07:39 +0000

    Problema detectado Sua configuração do tailwind.config.ts está limitando

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,10 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{ts,tsx}',
 	],
   prefix: "",
   theme: {

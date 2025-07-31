f133e0f468d01da6826714e3acd0c8c9a6b4d453
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-07-31 23:31:37 +0000

    Linting and checking validity of types ... Failed to compile. ./src/ai/f

--- src/ai/flows/consult-nfse-itajai.ts
+++ src/ai/flows/consult-nfse-itajai.ts
@@ -9,7 +9,7 @@
  */
 
 import { ai } from '@/ai/genkit';
-import { z, ZodType } from 'zod';
+import { z } from 'genkit';
 import { createClientAsync, Client } from 'soap';
 
 const ConsultNfseItajaiInputSchema = z.object({
@@ -30,7 +30,7 @@ const WSDL_URL = 'http://nfse-teste.publica.inf.br/homologa_nfse_integracao/Cons
 const consultNfseItajaiFlow = ai.defineFlow(
   {
     name: 'consultNfseItajaiFlow',
-    inputSchema: ConsultNfseItajaiInputSchema as ZodType<any>,
+    inputSchema: ConsultNfseItajaiInputSchema,
     outputSchema: ConsultNfseItajaiOutputSchema,
   },
   async (input) => {

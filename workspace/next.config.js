945ef365844b26608a68c1a59954a9976e30b758
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-08-01 13:34:00 +0000

    Unhandled Runtime Error Error: Invalid src prop (https://maps.google.com

--- workspace/next.config.js
+++ workspace/next.config.js
@@ -1,10 +1,29 @@
 
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   trailingSlash: true,
   skipTrailingSlashRedirect: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'maps.google.com',
+        port: '',
+        pathname: '/mapfiles/ms/icons/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'logo.clearbit.com',
+      }
+    ],
+  },
   transpilePackages: [
     'genkit',
     '@genkit-ai/ai',
@@ -13,13 +32,17 @@ const nextConfig = {
     '@genkit-ai/firebase',
     '@genkit-ai/google-cloud',
   ],
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.ignoreWarnings = [
         /require\.extensions/, 
         /Critical dependency/,
-        /Module not found: Can't resolve '@opentelemetry\/winston-transport'/,
-        /Module not found: Can't resolve '@opentelemetry\/exporter-jaeger'/
     ];
+    // These are optional dependencies of genkit, we can ignore them
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@opentelemetry/exporter-jaeger': false,
+      '@opentelemetry/winston-transport': false,
+    };
     return config;
   }
 }

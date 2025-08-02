d99bdc597921b2cfffafe80aee142b9634a71366
Camila Braga l LTi Global <cb@ltiglobal.com.br>
2025-08-02 04:32:32 +0000

    npm run build

--- src/components/partner-dialog.tsx
+++ src/components/partner-dialog.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState, useMemo, useRef, useEffect } from 'react';
-import { useForm, useFieldArray } from 'react-hook-form';
+import { useForm, useFieldArray, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import {
   Dialog,
